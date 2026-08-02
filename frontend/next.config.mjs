1e6e0ab8acd41b27905ec13c329b11cf6a8520ec
wwebtvmedia <wwebtvmedia@gmail.com>
2026-08-02 16:01:26 +0200

    Fix next.js version after audit downgrade and turbopack root

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  experimental: {
+    turbopack: {
+      root: "/home/pc/sby/investmenttools/frontend"
+    }
+  }
 };
 
 export default nextConfig;
